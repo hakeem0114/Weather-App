ef0475e3d0fae7a77f80b8fb10e97216f199c4e6
hakeem <hakeem0114@gmail.com>
2023-02-12 23:56:42 -0500

    Filtered out data for specific DOm elements

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ const weather = {
             fetch(
                 `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
             )
-            .then((response)=> response.json()) //Promise: then() for once url is fetched
-            .then((data)=> console.log(data)); //Check if Fetch pull worked
+            .then((response)=> response.json()) //Promise:then(). Waits till url data is fetched
+            .then((data)=> this.displayWeather(data)); //Check if Fetch pull worked & filter data using displayWeather()
         },
 
-        displayWeather: function(data){
-
+        displayWeather: function(data){ //Refer to openWeather docs to access proper names key:value pairs
+            const{name} = data; //Object destructuring to take property of object & assign it to a js parameter to re-use.
+            const {icon, description} = data.weather[0]; //Icon & description was in the 0th index of the weather array.
+            const {temp, humidity} = data.main;
+            const {speed} = data.wind;
+            console.log(name, icon, description, temp, humidity,speed );
         }
 }
