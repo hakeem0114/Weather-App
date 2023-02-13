713f2bd0666395fea87f0511d7e22e914b12f5d3
hakeem <hakeem0114@gmail.com>
2023-02-12 23:31:07 -0500

    Fetch API works. Received object from WeatherAPI & console logged it

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 //1000 request limit from OpenWeather
 
 const weather = { 
-        "apiKey": "393889fc2f3fe20f9b4e934b6d731e6f"
+        "apiKey": "393889fc2f3fe20f9b4e934b6d731e6f",
+
+        fetchWeather: function(){
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=393889fc2f3fe20f9b4e934b6d731e6f"
+            )
+            .then((response)=> response.json()) //Promise: then() for once url is fetched
+            .then((data)=> console.log(data)); //Check if Fetch pull worked
+        }
+    
 }
